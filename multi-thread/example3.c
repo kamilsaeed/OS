8d4807a4e4898914faf4f0de7319445f3959d9ad
Kamil Saeed <haseenasatti@Kamils-MacBook-Air-M1.local>
2025-10-15 09:05:03 +0500

    local variable passed by pointer

---
+++ multi-thread/example3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* modify_local(void* arg) 
+{
+    int* local_var_ptr = (int*) arg;
+
+    *local_var_ptr += 10;
+
+    printf("Thread: local_var = %d\n", *local_var_ptr); 
+
+    return NULL;
+}
+
+int main() 
+{
+    pthread_t thread;
+    int local_var = 20;
+
+    printf("Before thread: local_var = %d\n", local_var);
+
+    pthread_create(&thread, NULL, modify_local, (void*)&local_var);
+    pthread_join(thread, NULL);   
+
+    printf("After thread: local_var = %d\n", local_var);
+
+    return 0;
+}
