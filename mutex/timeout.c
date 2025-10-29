d1c756d7bcbda0c20ceea7e1e68e8dc118a80652
Kamil Saeed <kamilsaeedkamali18@gmail.com>
2025-10-29 10:26:14 +0500

    Deadlock resolved through timeout

--- mutex/timeout.c
+++ mutex/timeout.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 pthread_mutex_t lock1, lock2;
 
-void* thread1_func(void* arg) 
-{
+void* thread1_func(void* arg) {
     printf("Thread 1: Trying to lock mutex1...\n");
     pthread_mutex_lock(&lock1);
     printf("Thread 1: Locked mutex1.\n");
@@ -16,30 +17,44 @@ void* thread1_func(void* arg)
     pthread_mutex_lock(&lock2);
     printf("Thread 1: Acquired both locks.\n");
 
+    printf("Thread 1: Executing critical section.\n");
+    sleep(1);
+
     pthread_mutex_unlock(&lock2);
     pthread_mutex_unlock(&lock1);
     return NULL;
 }
 
-void* thread2_func(void* arg) 
-{
+void* thread2_func(void* arg) {
     printf("Thread 2: Trying to lock mutex2...\n");
     pthread_mutex_lock(&lock2);
     printf("Thread 2: Locked mutex2.\n");
 
     sleep(1);
+    printf("Thread 2: Trying to lock mutex1 (with simulated timeout)...\n");
 
-    printf("Thread 2: Trying to lock mutex1...\n");
-    pthread_mutex_lock(&lock1);
-    printf("Thread 2: Acquired both locks.\n");
+    int tries = 0, result;
+    while ((result = pthread_mutex_trylock(&lock1)) != 0 && tries < 10) {
+        printf("Thread 2: Waiting for mutex1... (%d)\n", tries + 1);
+        usleep(300000); // 0.3s delay
+        tries++;
+    }
 
-    pthread_mutex_unlock(&lock1);
+    if (result == 0) {
+        printf("Thread 2: Acquired both locks.\n");
+        printf("Thread 2: Executing critical section.\n");
+        pthread_mutex_unlock(&lock1);
+        printf("Thread 2: Releasing locks.\n");
+    } else {
+        printf("Thread 2: *** Timed out waiting for mutex1! ***\n");
+        printf("Thread 2: Backing off and releasing mutex2 to resolve deadlock.\n");
+    }
+
     pthread_mutex_unlock(&lock2);
     return NULL;
 }
 
-int main() 
-{
+int main() {
     pthread_t t1, t2;
     pthread_mutex_init(&lock1, NULL);
     pthread_mutex_init(&lock2, NULL);
@@ -52,6 +67,5 @@ int main()
 
     pthread_mutex_destroy(&lock1);
     pthread_mutex_destroy(&lock2);
-
     return 0;
 }
