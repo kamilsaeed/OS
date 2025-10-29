cd722218ee5bb06e502e4396cb9a67cf7f54f99a
Kamil Saeed <kamilsaeedkamali18@gmail.com>
2025-10-29 09:10:18 +0500

    multi-threading mutex

---
+++ mutex/mutex1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int balance = 0;
+pthread_mutex_t lock;
+
+void* deposit(void* arg) 
+{
+    for (int i = 0; i < 100000; i++) 
+    {
+        pthread_mutex_lock(&lock);
+        balance++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+void* withdraw(void* arg) 
+{
+    for (int i = 0; i < 10000; i++) 
+    {
+        pthread_mutex_lock(&lock);
+        balance--;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() 
+{
+    pthread_t t1, t2;
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, deposit, NULL);
+    pthread_create(&t2, NULL, withdraw, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Final Balance = %d\n", balance);
+
+    pthread_mutex_destroy(&lock);
+    return 0;
+}
