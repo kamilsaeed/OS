2bc0e5f41443605974391b13281cdead2cf7cc84
Kamil Saeed <kamilsaeedkamali18@gmail.com>
2025-11-12 09:36:12 +0500

    Basic Semaphore Synchronization

---
+++ semaphores/1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t sem1, sem2; 
+
+void* thread1_func(void* arg) 
+{
+    for (int i = 0; i < 5; i++) 
+    {
+        sem_wait(&sem1);    
+        printf("Thread 1: Message %d\n", i + 1);
+        sleep(1);
+        sem_post(&sem2);     
+    }
+    return NULL;
+}
+
+void* thread2_func(void* arg) 
+{
+    for (int i = 0; i < 5; i++) 
+    {
+        sem_wait(&sem2); 
+        printf("Thread 2: Message %d\n", i + 1);
+        sleep(1);
+        sem_post(&sem1);
+    }
+    return NULL;
+}
+
+int main() 
+{
+    pthread_t t1, t2;
+
+    sem_init(&sem1, 0, 1);  
+    sem_init(&sem2, 0, 0);  
+
+    pthread_create(&t1, NULL, thread1_func, NULL);
+    pthread_create(&t2, NULL, thread2_func, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&sem1);
+    sem_destroy(&sem2);
+
+    return 0;
+}
